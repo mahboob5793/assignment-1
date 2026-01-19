9d5254ae00809f8c26a181d5f9409b6b1080c09f
mahboob5793 <mahboob5793@gmail.com>
2026-01-19 17:03:06 -0500

    Fix ESLint typing issues in fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,16 +1,17 @@
 // Endpoint for querying the fibonacci numbers
-
+import { Request, Response } from "express";
 import fibonacci from "./fib";
 
-export default (req, res) => {
-  const { num } = req.params;
+export default function fibRoute(req: Request, res: Response): void {
+  const numStr: string = req.params.num;        // typed string
+  const num: number = parseInt(numStr, 10);     // typed number
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  const fibN: number = fibonacci(num);           // typed number
+  let result: string = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
   }
 
   res.send(result);
-};
+}
