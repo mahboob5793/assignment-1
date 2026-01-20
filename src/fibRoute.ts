fe199f59cb7b05948191803b3f358f5d7e282dec
mahboob5793 <58111629+mahboob5793@users.noreply.github.com>
2026-01-19 19:57:54 -0500

    Refactor result variable declaration in fibRoute

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -7,7 +7,7 @@ export default function fibRoute(req: Request, res: Response): void {
   const num: number = parseInt(numStr, 10);     // typed number
 
   const fibN: number = fibonacci(num);           // typed number
-  let result: string = `fibonacci(${num}) is ${fibN}`;
+  let result= `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
